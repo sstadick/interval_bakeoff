fc13f17714ab3ab0f9dfd40c20c1a43b764344d8
Seth Stadick <sstadick@gmail.com>
2019-09-18 21:45:59 -0700

    Working on fake data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #[macro_use]
 extern crate clap;
+use ailist::{self};
 use bio::data_structures::interval_tree::IntervalTree;
 use clap::{App, Arg, ArgMatches, SubCommand};
 use coitree::{COITree, IntervalNode};
 use cpu_time::ProcessTime;
 use nested_intervals::IntervalSet;
-use rand::prelude::*;
 use rand::Rng;
 use rust_lapper::{Interval, Lapper};
 use std::ops::Range;
@@ -17,8 +17,8 @@ arg_enum! {
         RustLapper,
         RustBio,
         NestedInterval,
-        //COITree,
-        //AIList,
+        COITree,
+        AIList,
         All,
     }
 }
@@ -130,6 +130,15 @@ fn run_fake(matches: &ArgMatches) {
     let save_sets = matches.value_of("save_sets");
     let libs: Vec<_> = values_t!(matches.values_of("lib"), Lib).unwrap_or(vec![Lib::All]);
 
+    println!("Config for fake run:");
+    println!("num_intervals\t{}", num_intervals);
+    println!("universe_size\t{}", universe_size);
+    println!("min_interval_size\t{}", min_interval_size);
+    println!("max_interval_size\t{}", max_interval_size);
+    println!("add_large_span\t{}", add_large_span);
+    println!("save_sets\t{:#?}", save_sets);
+    println!("libs\t{:#?}", libs);
+
     let (mut set_a, set_b) = make_intervals(
         num_intervals,
         universe_size,
@@ -149,10 +158,14 @@ fn run_fake(matches: &ArgMatches) {
             Lib::RustLapper => run_rust_lapper(&set_a, &set_b),
             Lib::RustBio => run_rust_bio(&set_a, &set_b),
             Lib::NestedInterval => run_nested_intervals(&set_a, &set_b),
+            Lib::COITree => run_coitree(&set_a, &set_b),
+            Lib::AIList => run_ailist(&set_a, &set_b),
             Lib::All => {
                 run_rust_lapper(&set_a, &set_b);
                 run_rust_bio(&set_a, &set_b);
                 run_nested_intervals(&set_a, &set_b);
+                run_coitree(&set_a, &set_b);
+                run_ailist(&set_a, &set_b);
             }
         }
     }
@@ -162,9 +175,11 @@ fn run_real(matches: &ArgMatches) {
     let bed_a = matches.value_of("bed_a").unwrap();
     let bed_b = matches.value_of("bed_b").unwrap();
     let libs: Vec<_> = values_t!(matches.values_of("lib"), Lib).unwrap_or(vec![Lib::All]);
+    unimplemented!();
 }
 
 fn run_rust_lapper(set_a: &Vec<Iv>, set_b: &Vec<Iv>) {
+    println!("rust-lapper");
     let set_a_intervals = set_a
         .iter()
         .map(|i| Interval {
@@ -185,19 +200,238 @@ fn run_rust_lapper(set_a: &Vec<Iv>, set_b: &Vec<Iv>) {
     let start = ProcessTime::now();
     let lapper_a = Lapper::new(set_a_intervals);
     let elapsed: Duration = start.elapsed();
-    println!("Time to create set a: {:#?}", elapsed);
+    println!("rust-lapper: Time to create set a: {:#?}", elapsed);
     let start = ProcessTime::now();
     let lapper_b = Lapper::new(set_b_intervals);
     let elapsed: Duration = start.elapsed();
-    println!("Time to create set b: {:#?}", elapsed);
+    println!("rust-lapper: Time to create set b: {:#?}", elapsed);
+
+    // Run A vs A
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in set_a.iter() {
+        count += lapper_a
+            .find(interval.start as usize, interval.stop as usize)
+            .count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "rust-lapper: 100% hit rate A vs A time/count: {:#?}/{}",
+        elapsed, count
+    );
+
+    // Run A vs B
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in set_a.iter() {
+        count += lapper_b
+            .find(interval.start as usize, interval.stop as usize)
+            .count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "rust-lapper: < 100% hit rate A vs B time/count: {:#?}/{}",
+        elapsed, count
+    );
+}
+
+fn run_ailist(set_a: &Vec<Iv>, set_b: &Vec<Iv>) {
+    println!("AIList");
+    let set_a_intervals = set_a
+        .iter()
+        .map(|i| {
+            (
+                ailist::Interval {
+                    start: i.start,
+                    stop: i.stop,
+                    max_end: 0,
+                },
+                0,
+            )
+        })
+        .collect();
+    let set_b_intervals = set_b
+        .iter()
+        .map(|i| {
+            (
+                ailist::Interval {
+                    start: i.start,
+                    stop: i.stop,
+                    max_end: 0,
+                },
+                0,
+            )
+        })
+        .collect();
+    // Object Creation
+    let start = ProcessTime::now();
+    let ailist_a = ailist::AIList::new(set_a_intervals, None, None);
+    let elapsed: Duration = start.elapsed();
+    println!("AIList: Time to create set a: {:#?}", elapsed);
+    let start = ProcessTime::now();
+    let ailist_b = ailist::AIList::new(set_b_intervals, None, None);
+    let elapsed: Duration = start.elapsed();
+    println!("AIList: Time to create set b: {:#?}", elapsed);
+
+    // Run A vs A
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in set_a.iter() {
+        count += ailist_a.find(interval.start, interval.stop).count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "AIList: 100% hit rate A vs A time/count: {:#?}/{}",
+        elapsed, count
+    );
+
+    // Run A vs B
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in set_a.iter() {
+        count += ailist_b.find(interval.start, interval.stop).count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "AIList: < 100% hit rate A vs B time/count: {:#?}/{}",
+        elapsed, count
+    );
+}
+
+fn run_coitree(set_a: &Vec<Iv>, set_b: &Vec<Iv>) {
+    println!("COITree");
+    let set_a_intervals = set_a
+        .iter()
+        .map(|i| IntervalNode::new(i.start as i32, i.stop as i32, 0))
+        .collect();
+    let set_b_intervals = set_b
+        .iter()
+        .map(|i| IntervalNode::new(i.start as i32, i.stop as i32, 0))
+        .collect();
+    // Object Creation
+    let start = ProcessTime::now();
+    let coitree_a = COITree::new(set_a_intervals);
+    let elapsed: Duration = start.elapsed();
+    println!("COITree: Time to create set a: {:#?}", elapsed);
+    let start = ProcessTime::now();
+    let coitree_b = COITree::new(set_b_intervals);
+    let elapsed: Duration = start.elapsed();
+    println!("COITree: Time to create set b: {:#?}", elapsed);
+
+    // Run A vs A
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in set_a.iter() {
+        count += coitree_a
+            .find(interval.start as i32, interval.stop as i32)
+            .count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "COITree: 100% hit rate A vs A time/count: {:#?}/{}",
+        elapsed, count
+    );
+
+    // Run A vs B
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in set_a.iter() {
+        count += coitree_b
+            .find(interval.start as i32, interval.stop as i32)
+            .count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "COITree: < 100% hit rate A vs B time/count: {:#?}/{}",
+        elapsed, count
+    );
 }
 
 fn run_rust_bio(set_a: &Vec<Iv>, set_b: &Vec<Iv>) {
-    unimplemented!();
+    println!("rust-bio");
+    let mut set_a_intervals = IntervalTree::new();
+    let mut set_b_intervals = IntervalTree::new();
+    let queries_a: Vec<Range<_>> = set_a.iter().map(|x| x.start..x.stop).collect();
+    // Object Creation
+    let start = ProcessTime::now();
+    set_a
+        .iter()
+        .for_each(|x| set_a_intervals.insert(x.start..x.stop, 0));
+    let elapsed: Duration = start.elapsed();
+    println!("rust-bio: Time to create set a: {:#?}", elapsed);
+    let start = ProcessTime::now();
+    set_b
+        .iter()
+        .for_each(|x| set_b_intervals.insert(x.start..x.stop, 0));
+    let elapsed: Duration = start.elapsed();
+    println!("rust-bio: Time to create set b: {:#?}", elapsed);
+
+    // Run A vs A
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in queries_a.iter() {
+        count += set_a_intervals.find(interval).count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "rust-bio: 100% hit rate A vs A time/count: {:#?}/{}",
+        elapsed, count
+    );
+
+    // Run A vs B
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in queries_a.iter() {
+        count += set_b_intervals.find(interval).count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "rust-bio: < 100% hit rate A vs B time/count: {:#?}/{}",
+        elapsed, count
+    );
 }
 
+// Cons: querys require mutability
+// Cons: no way to tag an interval
 fn run_nested_intervals(set_a: &Vec<Iv>, set_b: &Vec<Iv>) {
-    unimplemented!();
+    println!("nested-intervals");
+    let set_a_nested: Vec<Range<u32>> = set_a.iter().map(|x| x.start..x.stop).collect();
+    let set_b_nested: Vec<Range<u32>> = set_b.iter().map(|x| x.start..x.stop).collect();
+    let queries_a: Vec<Range<_>> = set_a.iter().map(|x| x.start..x.stop).collect();
+    let queries_b: Vec<Range<_>> = set_b.iter().map(|x| x.start..x.stop).collect();
+    // Object Creation
+    let start = ProcessTime::now();
+    let mut set_a_intervals = IntervalSet::new(&set_a_nested).unwrap();
+    let elapsed: Duration = start.elapsed();
+    println!("nested-intervals: Time to create set a: {:#?}", elapsed);
+    let start = ProcessTime::now();
+    let mut set_b_intervals = IntervalSet::new(&set_b_nested).unwrap();
+    let elapsed: Duration = start.elapsed();
+    println!("nested-intervals: Time to create set b: {:#?}", elapsed);
+
+    // Run A vs A
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in queries_a.iter() {
+        count += set_a_intervals.query_overlapping(interval).iter().count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "nested-intervals: 100% hit rate A vs A time/count: {:#?}/{}",
+        elapsed, count
+    );
+
+    // Run A vs B
+    let start = ProcessTime::now();
+    let mut count = 0;
+    for interval in queries_b.iter() {
+        count += set_b_intervals.query_overlapping(interval).iter().count();
+    }
+    let elapsed: Duration = start.elapsed();
+    println!(
+        "nested_intervals: < 100% hit rate A vs B time/count: {:#?}/{}",
+        elapsed, count
+    );
 }
 ///// Helpers / Setup functions
 
